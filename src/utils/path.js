e99fefe6c7967c5099c898a313b1c2d6581b1750
cuteXuan <1832251531@qq.com>
2020-07-20 18:00:06 +0800

    切换

--- src/utils/path.js
+++ src/utils/path.js
@@ -1,4 +1,4 @@
-const env = 'dev'
+const env = 'prop'
 const upPath = env === 'dev' ? 'http://192.168.1.3:9999' : 'http://47.93.238.152:80'
 const reqPath = env === 'dev' ? 'http://192.168.1.3:5001' : 'http://47.93.238.152:5001/'
 export {
